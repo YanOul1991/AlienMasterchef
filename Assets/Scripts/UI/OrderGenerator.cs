4f5ba21b217f13a35aed1832012a0873a533e1f7
Malaïka Abevi <e2395930@cmaisonneuve.qc.ca>
2025-12-18 14:10:46 -0500

    Avancement de la gestion des commandes  + UI

---
+++ Assets/Scripts/UI/OrderGenerator.cs
@@ -0,0 +1,97 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderGenerator : MonoBehaviour
+{
+    public OrderManager orderManager;
+    public Canvas prefabTicketOrder;
+    public List<TicketOrder> listTickets = new List<TicketOrder>();
+    List<Plat> listActiveOrders;
+    bool orderSent;
+
+    void Start()
+    {
+        orderSent = false;
+    }
+
+    void Update()
+    {
+        //Générer des commandes aux 30 secondes, pour un maximum de 3 commandes à la fois
+        listActiveOrders = orderManager.GetActiveOrders();
+        int nbCommandes = listActiveOrders.Count;
+
+        //S'il y a moins de 3 commandes, on démarre la coroutine pour 
+        if (nbCommandes < 3 && !orderSent)
+        {
+            orderSent = true;
+            StartCoroutine(GenererCommande());
+        }
+
+        //CompletionCommande();
+        //foreach(Ingredient i in (RecipeDatabase.GetRecipe(listActiveOrders[0].mealType)).requiredIngredients)
+        //{
+        //    //Liste des ingrédients requis
+        //    print(i.state);
+        //    print(i.baseIngredient);
+        //}
+        //print((RecipeDatabase.GetRecipe(listActiveOrders[0].mealType)).requiredIngredients);
+
+        Debug.Log("<color=blue>" + nbCommandes + "</color>");
+    }
+
+    //Transformer les types de EMeal en tableau
+    static T GetRandomEnum<T>()
+    {
+        System.Array A = System.Enum.GetValues(typeof(T));
+        T V = (T)A.GetValue(Random.Range(0, A.Length));
+        return V;
+    }
+
+    // Coroutine pour la création de commandes aux 30 secondes
+    private IEnumerator GenererCommande()
+    {
+        //Création de la commande aléatoirement
+        EMeal commandeAleatoire = GetRandomEnum<EMeal>();
+        orderManager.CreateOrder(commandeAleatoire);
+
+        //Affichage de la commande au comptoir (UI)
+
+        bool commandePasse = false;
+
+        for (int i = 0; i < listTickets.Count && i < listActiveOrders.Count; i++)
+        {
+            TicketOrder t = listTickets[i];
+
+            if (t.ticket == null && !commandePasse)
+            {
+                Canvas commandeGeneree = Instantiate(prefabTicketOrder, t.transform.position, t.transform.rotation);
+                commandeGeneree.gameObject.SetActive(true);
+                t.plat = listActiveOrders[i];
+                t.ticket = commandeGeneree;
+                commandePasse = true;
+            }
+            Debug.Log($"Item at index {i}: {t.plat}");
+        }
+
+        //Puis on attend 30 secondes
+        yield return new WaitForSeconds(30f);
+        orderSent = false;
+    }
+
+    //Ça fonctionne, juste définir la méthode de vérification
+    void CompletionCommande(Plat order)
+    {
+        orderManager.CompleteOrder(order);
+        foreach (TicketOrder t in listTickets)
+        {
+            if (t.plat.IsCompleted())
+            {
+                Destroy(t.ticket.gameObject);
+                t.ticket = null;
+                t.plat = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TicketCommande.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+
+public class TicketOrder
+{
+    public Transform transform;
+    public Canvas ticket;
+    public Plat plat;
+}
