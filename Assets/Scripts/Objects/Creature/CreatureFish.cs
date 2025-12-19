d611aafabc902c2beb5a9cc19f211718cb1417c7
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-12-18 20:07:00 -0500

    Update Network

--- Assets/Scripts/Objects/Creature/CreatureFish.cs
+++ Assets/Scripts/Objects/Creature/CreatureFish.cs
@@ -20,44 +20,30 @@ public class CreatureFish : Creature<CreatureFish>
   ---------------------------------- */
   [field: SerializeField] public override float BaseMovementSpeed { get; protected set; }
   [field: SerializeField] public override float PanicMovementSpeed { get; protected set; }
-  [field: SerializeField] public override FoodStateTransform[] FoodStateTransforms { get; protected set; }
   [field: SerializeField] public override GameObject ResultingFood { get; protected set; }
-
-  private static readonly WaitForSeconds s_DestinationCheckRate = new(0.01f);
 
   /* *******************
      * Components
   ******************* */
   private NavMeshAgent m_navAgent;
-  private Rigidbody m_rigidbody;
-  private Animator m_animator;
 
   public void Awake()
   {
-    m_animator = GetComponent<Animator>();
-    m_navAgent = GetComponent<NavMeshAgent>();
-    m_rigidbody = GetComponent<Rigidbody>();
+
+    if (TryGetComponent(out NavMeshAgent navMeshAgent)) m_navAgent = navMeshAgent;
+    else m_navAgent = (NavMeshAgent)gameObject.AddComponent(typeof(NavMeshAgent));
   }
 
   public override void OnNetworkSpawn()
   {
     base.OnNetworkSpawn();
-
-    if (!IsServer)
-    {
-      m_navAgent.enabled = false;
-    }
-
+    if (!IsServer) m_navAgent.enabled = false;
     Move();
   }
 
   private void OnTriggerEnter(Collider other)
   {
-    if (other.gameObject.name == "Knife")
-    {
-      // GetComponent<NetworkObject>().Despawn(true);
-      Death();
-    }
+    if (other.gameObject.name == "Knife") Death();
   }
 
   private Vector3 GetRandomPoint()
@@ -66,9 +52,7 @@ public class CreatureFish : Creature<CreatureFish>
     randomPoint += transform.position;
 
     if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 5.0f, NavMesh.AllAreas))
-    {
       return hit.position;
-    }
 
     return transform.position;
   }
@@ -76,13 +60,11 @@ public class CreatureFish : Creature<CreatureFish>
   /* ----------------------------------
     --- Overrides | Methods
   ---------------------------------- */
-  public override void InteractionListen(IInteractionTriggerer triggerer) { }
 
   protected override void Death() 
   {
     if (!IsServer) return;
-    // SpawnResultingFood();
-    
+
     NetworkServer.DeactivateGameObjectInNetwork(gameObject);
     GameObject _food = Instantiate(ResultingFood, transform.position, transform.rotation);
     NetworkServer.SpawnGameObjectInNetwork(_food);
@@ -99,22 +81,15 @@ public class CreatureFish : Creature<CreatureFish>
 
   protected override void Panic() { }
 
-  // private void SpawnResultingFood()
-  // {
-  //   GameObject instance = Instantiate(ResultingFood);
-  //   instance.transform.position = transform.position;
-  //   Destroy(gameObject);
-  // }
-
   private IEnumerator CheckForDestination()
   {
     while (Vector3.Distance(m_navAgent.destination, transform.position) > 0.1f)
-    {
       yield return null;
-    }
 
     yield return new WaitForSeconds(Random.Range(0, 2.0f));
+
     Move();
+
     yield break;
   }
 }
